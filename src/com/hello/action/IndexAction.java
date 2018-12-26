e002a3e4ae120e20c17fb12185b6b24bee13a4a2
kys <ring321@naver.com>
2018-12-26 21:12:29 +0900

    fistcomit

---
+++ src/com/hello/action/IndexAction.java
@@ -0,0 +1,13 @@
+package com.hello.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexAction implements Action{
+	public String process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		return "/info.jsp";
+	}
+}
